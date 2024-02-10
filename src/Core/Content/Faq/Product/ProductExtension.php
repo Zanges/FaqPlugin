c296b102020c26a346597d18816bbc1062f4e641
Zanges <3868054+Zanges@users.noreply.github.com>
2024-02-10 17:47:35 +0100

    Add ProductExtension to handle faqs association

---
+++ src/Core/Content/Faq/Product/ProductExtension.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace FaqPlugin\Core\Content\Faq\Product;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityExtension;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+use Shopware\Core\Content\Product\ProductDefinition;
+use FaqPlugin\Core\Content\Faq\FaqDefinition;
+
+class ProductExtension extends EntityExtension
+{
+    public function extendFields(FieldCollection $collection): void
+    {
+        $collection->add(
+            new ManyToOneAssociationField('faqs', 'id', 'product_id', FaqDefinition::class, true)
+        );
+    }
+
+    public function getDefinitionClass(): string
+    {
+        return ProductDefinition::class;
+    }
+}
